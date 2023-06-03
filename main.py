9e97da588fbd29cac9b228bc7e5a0eb90293b7bf
Prasenjit Sarkar <pro.prasenjit@gmail.com>
2023-06-03 08:38:02 +0530

    access all from main
    import all module exercises to main.py

--- main.py
+++ main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while True:
+    a = int(input("""
+    ==========================================================
+    0. Exit
+    1. Exercise 1
+    2. Exercise 2
+    3. Exercise 3
+    4. Exercise 4
+    .
+    .
+    .
+    .
+    32. Exercise 32
+    """))
+    if a == 1:
+        import Exercise001
+    elif a == 2:
+        import Exercise002
+    elif a == 3:
+        import Exercise003
+    elif a == 4:
+        import Exercise004
+    elif a == 5:
+        import Exercise005
+    elif a == 6:
+        import Exercise006
+    elif a == 7:
+        import Exercise007
+    elif a == 8:
+        import Exercise008
+    elif a == 9:
+        import Exercise009
+    elif a == 10:
+        import Exercise0010
+    elif a == 11:
+        import Exercise011
+    elif a == 12:
+        import Exercise012
+    elif a == 13:
+        import Exercise013
+    elif a == 14:
+        import Exercise014
+    elif a == 15:
+        import Exercise015
+    elif a == 16:
+        import Exercise016
+    elif a == 17:
+        import Exercise017
+    elif a == 18:
+        import Exercise018
+    elif a == 19:
+        import Exercise019
+    elif a == 20:
+        import Exercise020
+    elif a == 21:
+        import Exercise021
+    elif a == 22:
+        import Exercise022
+    elif a == 23:
+        import Exercise024
+    elif a == 24:
+        import Exercise024
+    elif a == 25:
+        import Exercise025
+    elif a == 26:
+        import Exercise026
+    elif a == 27:
+        import Exercise027
+    elif a == 28:
+        import Exercise028
+    elif a == 29:
+        import Exercise029
+    elif a == 30:
+        import Exercise030
+    elif a == 31:
+        import Exercise031
+    elif a == 32:
+        import Exercise032
